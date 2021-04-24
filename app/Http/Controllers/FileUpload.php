e392524e2e90e56600ebec68400012f205d965f2
goussous0 <73262976+goussous0@users.noreply.github.com>
2021-04-24 18:22:05 -0400

    Add files via upload

---
+++ app/Http/Controllers/FileUpload.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\File;
+
+
+class FileUpload extends Controller
+{
+  public function createForm(){
+    return view('file-upload');
+  }
+
+  public function fileUpload(Request $req){
+        $req->validate([
+        'file' => 'required|mimes:csv,txt,xlx,xls,jpg,png,pdf|max:4096'
+        ]);
+
+        $fileModel = new File;
+
+        if($req->file()) {
+            $fileName = time().'_'.$req->file->getClientOriginalName();
+            $filePath = $req->file('file')->storeAs('uploads', $fileName, 'public');
+
+            $fileModel->name = time().'_'.$req->file->getClientOriginalName();
+            $fileModel->file_path = '/storage/' . $filePath;
+            $fileModel->save();
+
+            return back()
+            ->with('success','File has been uploaded.')
+            ->with('file', $fileName);
+        }
+   }
+
+}
