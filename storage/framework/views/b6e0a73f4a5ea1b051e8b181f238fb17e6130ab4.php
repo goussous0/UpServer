135137543ec032020082c5ccead898a31597e947
goussous0 <73262976+goussous0@users.noreply.github.com>
2021-04-25 03:17:53 +0300

    Add files via upload

---
+++ storage/framework/views/b6e0a73f4a5ea1b051e8b181f238fb17e6130ab4.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    <title>Laravel File Upload</title>
+    <style>
+        .container {
+            max-width: 500px;
+        }
+        dl, ol, ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <form action="<?php echo e(route('fileUpload')); ?>" method="post" enctype="multipart/form-data">
+          <h3 class="text-center mb-5">Upload File in Laravel</h3>
+            <?php echo csrf_field(); ?>
+            <?php if($message = Session::get('success')): ?>
+            <div class="alert alert-success">
+                <strong><?php echo e($message); ?></strong>
+            </div>
+          <?php endif; ?>
+
+          <?php if(count($errors) > 0): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                      <li><?php echo e($error); ?></li>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+            </div>
+          <?php endif; ?>
+
+            <div class="custom-file">
+                <input type="file" name="file" class="custom-file-input" id="chooseFile">
+                <label class="custom-file-label" for="chooseFile">Select file</label>
+            </div>
+
+            <button type="submit" name="submit" class="btn btn-primary btn-block mt-4">
+                Upload Files
+            </button>
+        </form>
+    </div>
+
+</body>
+</html>
+<?php /**PATH /home/kali/Desktop/pr0j/resources/views/file-upload.blade.php ENDPATH**/ ?>
